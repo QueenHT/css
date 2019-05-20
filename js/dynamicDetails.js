973b7d7f4563e6c35775d49a2780fa7e72b2422f
QueenHT <997418473@qq.com>
2019-05-20 09:05:01 +0800

    modify

--- js/dynamicDetails.js
+++ js/dynamicDetails.js
@@ -9,41 +9,62 @@ for(var i =0; i < $inputs.length; i++)
 {  
     listenKeybord($inputs[i]);
 }
-function commentClick(){
-    $('#commentInp').focus();
-    alert('window',$(window).height())
-   
- 
-}
-if (checkSystem()) {
-     
+// 监听输入框的软键盘弹起和收起事件
+function listenKeybord($input){
+    if(judgeDeviceType.isIOS){    
 // IOS 键盘弹起：IOS 和 Android 输入框获取焦点键盘弹起
-
+    $input.addEventListener('focus',function(){
+      console.log('IOS 键盘弹起啦！');      
+// IOS 键盘弹起后操作    
+},false)    
+// IOS 键盘收起：IOS 点击输入框以外区域或点击收起按钮，输入框都会失去焦点，键盘会收起，
+    $input.addEventListener('blur',()=>{
+              console.log('IOS 键盘收起啦！');      
+// IOS 键盘收起后操作    
+})  
+}
+// function commentClick(){
+//     $('#commentInp').focus();
+//     alert('window',$(window).height())   
+//     $('.comment-input-div').css('display',none)
+//     }
+if (checkSystem()) {     
+// IOS 键盘弹起：IOS 和 Android 输入框获取焦点键盘弹起
 $input.addEventListener('focus', function(){
       alert('IOS 键盘弹起啦！');      
 // IOS 键盘弹起后操作    
-},false)    
+},false)
 // IOS 键盘收起：IOS 点击输入框以外区域或点击收起按钮，输入框都会失去焦点，键盘会收起，
     $input.addEventListener('blur', () => {
              alert('IOS 键盘收起啦！');      
 // IOS 键盘收起后操作    
 })
 }else{
-
-     var originHeight=document.documentElement.clientHeight||document.body.clientHeight;  
-    window.addEventListener('resize',function(){ 
-    var  resizeHeight  = document.documentElement.clientHeight||document.body.clientHeight; 
+ var originHeight=document.documentElement.clientHeight||document.body.clientHeight;  
+ window.addEventListener('resize',function(){ 
+ var  resizeHeight  = document.documentElement.clientHeight||document.body.clientHeight; 
    if(originHeight<resizeHeight) 
    {  
-          alert('Android 键盘收起啦！');
+       alert('Android 键盘收起啦！');
    // Android 键盘收起后操作         
-   }else{
-   
-           alert('Android 键盘弹起啦！' );  
-           
-   // Android 键盘弹起后操作  
-         
+   }else{   
+      alert('Android 键盘弹起啦！' );             
+   // Android 键盘弹起后操作           
    }    
     originHeight= resizeHeight;        
    },false)
 }
+function activeElementScrollIntoView(activeElement,delay){  
+var editable = activeElement.getAttribute('contenteditable')  
+// 输入框、textarea或富文本获取焦点后没有将该元素滚动到可视区  
+if(activeElement.tagName=='INPUT'||activeElement.tagName=='TEXTAREA'||editable===''||editable)
+    {
+    setTimeout(function()
+        {activeElement.scrollIntoView();
+        },delay) 
+    }
+}
+
+// ...
+// Android 键盘弹起后操作
+activeElementScrollIntoView($input,1000);
